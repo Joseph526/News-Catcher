0b352ae59dd262b5326ff38343aeb96944d72913
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-17 15:24:19 -0400

    Set up server

---
+++ server.js
@@ -0,0 +1,48 @@
+// Require dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Set up Express app
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("./public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Require Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Require all models
+var db = require("./models");
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+
+// Connect to the Mongo DB
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+// Require routes for API endpoints
+var routes = require("./controllers/articles_controller.js");
+
+app.use(routes);
+
+// Start the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
