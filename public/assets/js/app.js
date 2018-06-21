66667547d2b4bca23e872c42afd03d5c1573682f
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-20 23:18:22 -0400

    Notes form submit and DB push working

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -10,7 +10,28 @@ $(document).ready(function() {
     });
 
     $("button.notes").on("click", function(event) {
+        // Grab the id of the particular Notes button
         var id = $(this).data("btn-id");
+        // Toggle the div with matching id
         $("div.notes#" + id).slideToggle("slow");
+    });
+
+    $("button.noteSubmit").on("click", function(event) {
+        event.preventDefault();
+        
+        // Grab the id of the particular Submit button
+        var id = $(this).attr("id");
+        // Grab the user input data
+        var noteData = {
+            title: $("#noteTitle" + id).val().trim(),
+            body: $("#noteBody" + id).val().trim()
+        };
+        console.log(noteData);
+
+        // Send the POST request
+        $.post("/articles/" + id, noteData)
+            .then(function(data) {
+                console.log(data);
+            });
     });
 });
